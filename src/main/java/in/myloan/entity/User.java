3e05b891811b2eb9b2d2556facf89027a9c884cd
Himanshu Deshmukh <himanshu.deshmukh612@gmail.com>
2024-10-07 13:42:04 +0530

    minor changes

--- src/main/java/in/myloan/entity/User.java
+++ src/main/java/in/myloan/entity/User.java
@@ -1,11 +1,12 @@
 package in.myloan.entity;
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@Table(name="usertb")
 public class User {
 
 	@Id
